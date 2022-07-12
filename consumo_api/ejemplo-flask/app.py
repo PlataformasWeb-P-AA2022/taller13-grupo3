d43d8c10bfa4529907e686b7a077c271aa1c0829
djleiva2000 <diegoleiva140@gmail.com>
2022-07-12 14:00:48 -0500

    cambios

--- consumo_api/ejemplo-flask/app.py
+++ consumo_api/ejemplo-flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import requests
 import json
-
+from config import usuario, clave
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
@@ -14,7 +14,7 @@ def los_estudiantes():
     """
     """
     r = requests.get("http://127.0.0.1:8000/api/estudiantes/",
-            auth=('user', 'pass'))
+            auth=(usuario, clave))
     estudiantes = json.loads(r.content)['results']
     numero_estudiantes = json.loads(r.content)['count']
     return render_template("losestudiantes.html", estudiantes=estudiantes,
@@ -26,7 +26,7 @@ def los_telefonos():
     """
     """
     r = requests.get("http://127.0.0.1:8000/api/numerost/",
-            auth=('user', 'pass'))
+            auth=(usuario, clave))
     datos = json.loads(r.content)['results']
     numero = json.loads(r.content)['count']
     return render_template("lostelefonos.html", datos=datos,
@@ -38,13 +38,12 @@ def los_telefonos_dos():
     """
     """
     r = requests.get("http://127.0.0.1:8000/api/numerost/",
-            auth=('user', 'pass'))
+            auth=(usuario, clave))
     datos = json.loads(r.content)['results']
     numero = json.loads(r.content)['count']
     datos2 = []
     for d in datos:
-        datos2.append({'telefono':d['telefono'], 'tipo':d['tipo'],
-        'estudiante': obtener_estudiante(d['estudiante'])})
+        datos2.append({'telefono':d['telefono'], 'tipo':d['tipo'], 'estudiante': obtener_estudiante(d['estudiante'])})
     return render_template("lostelefonosdos.html", datos=datos2,
     numero=numero)
 
@@ -53,6 +52,8 @@ def los_telefonos_dos():
 def obtener_estudiante(url):
     """
     """
-    r = requests.get(url, auth=('user', 'pass'))
-    nombre_estudiante = json.loads(r.content)['nombre']
-    return nombre_estudiante
+    r = requests.get(url, auth=(usuario, clave))
+    nombre_estudiante = json.loads(r.content)['nombre' ]
+    apellido_estudiante = json.loads(r.content)['apellido' ]
+    cadena = "%s %s" % (nombre_estudiante, apellido_estudiante)
+    return cadena
