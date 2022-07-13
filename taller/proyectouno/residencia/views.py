b4bde856a55de3e8f68f440428296b55986862a8
Jhoselin Guachizaca <jaguachizaca@gmail.com>
2022-07-12 19:08:51 -0500

    TallerG

---
+++ taller/proyectouno/residencia/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render
+
+# importar las clases de models.py
+from residencias.models import *
+
+# importar los formularios de forms.py
+from residencias.forms import *
+
+# Create your views here.
+
+def index(request):
+
+    edificios = Edificio.objects.all()
+
+    informacion_template = {'edificios': edificios, 'numero_edificios': len(edificios)}
+    return render(request, 'index.html', informacion_template)
+
+
+def obtener_edificio(request, id):
+
+    edificio = Edificio.objects.get(pk=id)
+
+    informacion_template = {'edificio': edificio}
+    return render(request, 'obtener_edificio.html', informacion_template)
+
+
+def crear_edificio(request):
+
+    if request.method=='POST':
+        formulario = EdificioForm(request.POST)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save() # se guarda en la base de datos
+            return redirect(index)
+    else:
+        formulario = EdificioForm()
+    diccionario = {'formulario': formulario}
+
+    return render(request, 'crear_edificio.html', diccionario)
+
+
+def editar_edificio(request, id):
+
+    edificio = Edificio.objects.get(pk=id)
+    if request.method=='POST':
+        formulario = EdificioForm(request.POST, instance=edificio)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(index)
+    else:
+        formulario = EdificioForm(instance=edificio)
+    diccionario = {'formulario': formulario}
+
+    return render(request, 'editar_edificio.html', diccionario)
+
+
+def eliminar_edificio(request, id):
+    """
+    """
+    edificio = Edificio.objects.get(pk=id)
+    edificio.delete()
+    return redirect(index)
+
+
+def crear_departamento(request):
+
+    if request.method=='POST':
+        formulario = DepartamentoForm(request.POST)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(index)
+    else:
+        formulario = DepartamentoForm()
+    diccionario = {'formulario': formulario}
+
+    return render(request, 'crear_departamento.html', diccionario)
+
+
+def editar_departamento(request, id):
+
+    departamento = Departamento.objects.get(pk=id)
+    if request.method=='POST':
+        formulario = DepartamentoForm(request.POST, instance=departamento)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(index)
+    else:
+        formulario = DepartamentoForm(instance=departamento)
+    diccionario = {'formulario': formulario}
+
+    return render(request, 'crear_departamento.html', diccionario)
+
+def crear_departamento_edificio(request, id):
+
+    edificio = Edificio.objects.get(pk=id)
+    if request.method=='POST':
+        formulario = DepartamentoEdificioForm(edificio, request.POST)
+        print(formulario.errors)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(index)
+    else:
+        formulario = DepartamentoEdificioForm(edificio)
+    diccionario = {'formulario': formulario, 'edificio': edificio}
+
+    return render(request, 'crear_departamento_edificio.html', diccionario)
