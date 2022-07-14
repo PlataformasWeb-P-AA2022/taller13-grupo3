48711e5b5f1910359e832958899b734c652b5af0
Jhoselin Guachizaca <jaguachizaca@gmail.com>
2022-07-13 20:01:36 -0500

    TallerG

--- taller/ejemplo-flask/app.py
+++ taller/ejemplo-flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import requests
 import json
-from config import user, password
+from config import usuario, clave
 
 app = Flask(__name__, template_folder='templates')
 
